303492ec53f8f4eb337043fcfb2a5188d05f8283
Eric Gu <eric.changning.gu@gmail.com>
2018-07-02 22:25:13 -0400

    added zillow logo

--- dashboard.py
+++ dashboard.py
@@ -2,57 +2,24 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import quandl
-import config
+import zillow as z
 
 app = dash.Dash()
+app.css.append_css({
+    'external_url': (
+        'https://cdn.rawgit.com/chriddyp/0247653a7c52feb4c48437e1c1837f75'
+        '/raw/a68333b876edaf62df2efa7bac0e9b3613258851/dash.css'
+    )
+})
 
 app.layout = html.Div(children=[
-    dcc.Input(id='input', value='', type='text'),
-    html.Div(children='First Zip Code'),
-    html.Div(id='output-graph'),
+     html.Div([
+            html.Img(src='https://cdn.geekwire.com/wp-content/uploads/2015/02/Zillow-box-logo.png', height='100', width='100')
+        ], className="two columns padded"),
+    html.H1('Zillow Real Estate Research', style={'text-align': 'center'}),
+    html.H3('Median Rental Price Per Square Foot Over Last 3 Years by U.S. Zip Code', style={'text-align': 'center'})
 
 ])
-
-@app.callback(
-    Output(component_id='output-graph', component_property='children'),
-    [Input(component_id='input', component_property='value')]
-)
-def update_value(input_data):
-    quandl.ApiConfig.api_key = config.api_key
-    parameters = {
-        'start_date': '2016-01-31',
-        'end_date': '2018-05-31',
-        'metric': 'MRPFAH', # median rent price /sq foot,
-        'zipcode': input_data,
-        'zipcode1': '06903'
-    }
-
-    METRIC = 'ZILLOW/Z{0}_{1}'.format(parameters["zipcode"], parameters["metric"])
-
-    rent_price_data = quandl.get(METRIC, start_date=parameters["start_date"], end_date=parameters["end_date"])
-   def get_data(self):
-        quandl.ApiConfig.api_key = config.api_key
-        request = 'ZILLOW/{0}{1}_{2}'.format(self.code_segment, self.code, self.metric)
-        return quandl.get(METRIC, start_date=self.start_date, end_date=self.end_date)
-
-            def __str__(self):
-        data_string = self.data[:10]
-        return str(data_string)
-
-    return dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-            {'x': rent_price_data.index, 'y': rent_price_data['Value'], 'type': 'line', 'name': input_data},
-            {'x': rent_price_data.index, 'y': rent_price_data['Value']+1, 'type': 'line', 'name': input_data},
-            ],
-            'layout': {
-                'title': 'Real Estate By Year'
-            }
-        }
-    )
-
 
 
 
