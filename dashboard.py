2d4922245d587b0159586802a136ff45c4aaca50
Eric Gu <eric.changning.gu@gmail.com>
2018-07-02 21:41:44 -0400

    completed zillow class

--- dashboard.py
+++ dashboard.py
@@ -9,8 +9,8 @@ app = dash.Dash()
 
 app.layout = html.Div(children=[
     dcc.Input(id='input', value='', type='text'),
-    html.Div(id='output-graph'),
     html.Div(children='First Zip Code'),
+    html.Div(id='output-graph'),
 
 ])
 
@@ -30,16 +30,22 @@ def update_value(input_data):
 
     METRIC = 'ZILLOW/Z{0}_{1}'.format(parameters["zipcode"], parameters["metric"])
 
-    try:
-        rent_price_data = quandl.get(METRIC, start_date=parameters["start_date"], end_date=parameters["end_date"])
-    except:
-        pass  
+    rent_price_data = quandl.get(METRIC, start_date=parameters["start_date"], end_date=parameters["end_date"])
+   def get_data(self):
+        quandl.ApiConfig.api_key = config.api_key
+        request = 'ZILLOW/{0}{1}_{2}'.format(self.code_segment, self.code, self.metric)
+        return quandl.get(METRIC, start_date=self.start_date, end_date=self.end_date)
+
+            def __str__(self):
+        data_string = self.data[:10]
+        return str(data_string)
 
     return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': rent_price_data.index, 'y': rent_price_data['Value'], 'type': 'line', 'name': input_data},
+            {'x': rent_price_data.index, 'y': rent_price_data['Value'], 'type': 'line', 'name': input_data},
+            {'x': rent_price_data.index, 'y': rent_price_data['Value']+1, 'type': 'line', 'name': input_data},
             ],
             'layout': {
                 'title': 'Real Estate By Year'
---
+++ zillow.py
@@ -0,0 +1,30 @@
+import quandl
+import config
+import datetime 
+from dateutil.relativedelta import relativedelta
+
+class Zillow:
+   
+    def __init__(self, code, code_segment, metric, time_span):
+        self.code = code
+        self.code_segment = code_segment
+        self.metric = metric
+        self.time_span = time_span
+        self.end_date = datetime.datetime.today().strftime('%Y-%m-%d')
+        self.start_date = (datetime.datetime.now() - relativedelta(years=self.time_span)).strftime('%Y-%m-%d')
+        self.data = self.getData() 
+    
+    def __str__(self):
+        data_string = self.data[:25]
+        return str(data_string)
+
+    def getData(self):
+        quandl.ApiConfig.api_key = config.api_key
+        request = 'ZILLOW/{0}{1}_{2}'.format(self.code_segment, self.code, self.metric)
+        return quandl.get(request, start_date=self.start_date, end_date=self.end_date)
+
+if __name__ == "__main__":
+    zip1 = ZillowDataObject('06902', 'Z', 'MRPFAH', 3)
+    print(zip1.end_date)
+    print(zip1.start_date)
+    print(zip1)
